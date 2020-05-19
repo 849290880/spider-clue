cf9278bdc33f8f7dcf04b7933304b6e1fe254979
wuhaichao <849290880@qq.com>
2020-05-19 14:43:17 +0800

    类的实例

---
+++ crawl/clue-spider.py
@@ -0,0 +1,37 @@
+from peewee import *
+
+db = MySQLDatabase('py-test')
+
+
+def test1():
+    pass
+
+
+class people:
+    def printPeople(self):
+        print("hello people")
+
+
+def displayCount():
+    print("Total Employee")
+
+
+class Employee:
+    # '所有员工的基类'
+    empCount = 0
+
+    def __init__(self, name='', salary=''):
+        self.name = name
+        self.salary = salary
+        Employee.empCount += 1
+
+    def displayCount(self):
+        print("Total Employee %d" % Employee.empCount)
+
+    def displayEmployee(self):
+        print("Name : ", self.name, ", Salary: ", self.salary)
+
+
+if __name__ == '__main__':
+    e = Employee()
+    e.displayCount()
