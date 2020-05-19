ad2f52e2423b7148470f915a16bb85124f6dac85
wuhaichao <849290880@qq.com>
2020-05-19 16:16:27 +0800

    对象销毁测试

--- crawl/clue-spider.py
+++ crawl/clue-spider.py
@@ -16,8 +16,8 @@ def displayCount():
     print("Total Employee")
 
 
-class Employee:
-    # '所有员工的基类'
+class Employee(people):
+    '所有员工的基类'
     empCount = 0
 
     def __init__(self, name='', salary=''):
@@ -31,7 +31,8 @@ class Employee:
     def displayEmployee(self):
         print("Name : ", self.name, ", Salary: ", self.salary)
 
-
 if __name__ == '__main__':
-    e = Employee()
-    e.displayCount()
+    e1 = Employee()
+    e2 = Employee()
+    print(id(e1))
+    print(id(e2))
